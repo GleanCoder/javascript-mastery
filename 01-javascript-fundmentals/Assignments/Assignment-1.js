656b010b158f2a188716d0c93a7cd4b2a18d17ee
GleanCoder <adityakiranacharya0@gmail.com>
2025-05-23 21:45:47 +0530

    values and variables assignment is done and added into the folder

---
+++ 01-javascript-fundmentals/Assignments/Assignment-1.js
@@ -0,0 +1,10 @@
+// 1.Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+const country = "India";
+const continent = "Asia";
+let population = 150;
+
+// 2. Log their values to the console.
+console.log(country);
+console.log(continent);
+console.log(population);
